b7a8ce5c5772ec286a8dc5fd0db9142afacca674
devotion <303054578@qq.com>
2019-07-07 19:32:34 +0800

    jar版本升级

--- eureka-client/src/main/java/top/wugy/cloud/eureka/client/EurekaClientApp.java
+++ eureka-client/src/main/java/top/wugy/cloud/eureka/client/EurekaClientApp.java
@@ -3,6 +3,7 @@ package top.wugy.cloud.eureka.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * guangyao.wu 2019/7/6 17:04
